115d4aa6c193480ef5cf9425f2f5de99f5e76485
aondodawid <aondo1@gmail.com>
2025-05-04 02:25:07 +0200

    console.log cleaned

--- src/pwa/sw.js
+++ src/pwa/sw.js
@@ -19,8 +19,6 @@ const notification = SETTINGS.notification;
 const saveSubscriptionPath = SETTINGS.saveSubscriptionPath;
 const applicationServerKey = SETTINGS.applicationServerKey;
 
-console.log("🚀 ~ notification:", notification);
-
 async function runNotifications() {
   const urlBase64ToUint8Array = (base64String) => {
     const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
