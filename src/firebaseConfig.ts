b2c69c509f23315bd817a128d58faa6b5065ed6c
aondodawid <aondo1@gmail.com>
2025-08-03 07:26:39 +0200

    bugs fixed

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -7,18 +7,22 @@ type PWAOptions = {
   fcmServiceAccountKey: object;
 };
 
+let messaging = null;
+let auth = null;
+
 const fcmServiceAccountKey =
   (pwaOptions?.fcmServiceAccountKey as PWAOptions["fcmServiceAccountKey"]) || null;
+if (fcmServiceAccountKey) {
+  // Only initialize if no apps have been initialized yet
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(fcmServiceAccountKey as admin.ServiceAccount),
+    });
+  }
 
-// Only initialize if no apps have been initialized yet
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(fcmServiceAccountKey as admin.ServiceAccount),
-  });
+  auth = admin.auth();
+
+  messaging = admin.messaging();
 }
-
-const auth = admin.auth();
-
-const messaging = admin.messaging();
 
 export { messaging, auth, signInWithCustomToken };
